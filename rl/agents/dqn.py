9122579c8342c62d7a2072fbae7dfe6216e4f59f
Torsten Wörtwein <twoertwein@gmail.com>
2017-01-13 21:33:14 -0500

    some changes

--- rl/agents/dqn.py
+++ rl/agents/dqn.py
@@ -3,7 +3,6 @@ sys.path.append('../scripts')
 
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.ndimage.morphology import binary_erosion
 
 import gym
 
@@ -14,13 +13,14 @@ from preprocess import get_positions, get_state
 from experience_buffer import experience_buffer
 
 number_of_actions = 6
+length_of_states = 4
 
 def run():
     # init keras
     model = init_model()
-    buffer = experience_buffer(buffer_size = 10000)
+    main_buffer = experience_buffer(buffer_size=1000, reward_index=5)
     batch_size = 100
-    decay = 0.5;
+    decay = 0.9
 
     # init environment
     # 0,1 -> stay
@@ -29,31 +29,56 @@ def run():
     env = gym.make('Pong-v0')
     s_t = get_state(get_positions(env.reset()))
 
-    for _ in range(1000):
+    for i in range(1000):
+        print 'Episode', i
 
-        for _ in range(batch_size*1):
-            # select action a
-            # argmax a Q(state, action)
-            a_t = np.argmax(get_q_values(model, s_t))
+        for _ in range(5):
+            replay_buffer = np.empty((batch_size, length_of_states*2+2))
 
-            # take action and get reward
-            env.render()
-            (o_t1, r_t, done, _) = env.step(a_t)
+            for j in range(batch_size):# better than random; stack states; read about q learning
+                # select action a
+                if i < 50:
+                    if np.random.randint(0, 5) == 0:
+                        a_t = np.argmax(get_q_values(model, s_t))
+                        print 'argmax:', a_t
+                    else:
+                        distance = s_t[0] - s_t[-1]
+                        if distance > 0.2:
+                            a_t = 2
+                        elif distance > 0.08:
+                            a_t = 4
+                        elif distance < -0.2:
+                            a_t = 3
+                        elif distance < -0.08:
+                            a_t = 5
+                        else:
+                            if np.random.randint(0, 2) == 0:
+                                a_t = 0
+                            else:
+                                a_t = 1
+                else:
+                    # argmax a Q(state, action)
+                    a_t = np.argmax(get_q_values(model, s_t))
 
-            # get state
-            pos_t1 = get_positions(o_t1)
-            s_t1 = get_state(pos_t1)
+                # take action and get reward
+                env.render()
+                (o_t1, r_t, done, _) = env.step(a_t)
 
-            # replay memory
-            buffer.add(pacakge_replay(s_t, a_t, r_t, s_t1))
+                # get state
+                s_t1 = get_state(get_positions(o_t1))
 
-            s_t = s_t1
+                # replay memory
+                replay_buffer[j, :] = pacakge_replay(s_t, a_t, r_t, s_t1)
 
-            # reset env
-            if done:
-                s_t = get_state(get_positions(env.reset()))
-        
-        minibatch = buffer.sample(batch_size)
+                s_t = s_t1
+
+                # reset env
+                if done:
+                    s_t = get_state(get_positions(env.reset()))
+
+            main_buffer.add(replay_buffer)
+
+        minibatch = main_buffer.sample(batch_size)
 
         # create targets (no terminal state in pong)
         tts = np.zeros((minibatch.shape[0], 1))
@@ -70,39 +95,38 @@ def run():
     env.close()
 
 def merge_state_action(s_t, a_t):
-    input = np.zeros((1, s_t.size+1))
-    input[-1] = a_t
-    return input
+    merged = np.zeros((1, s_t.size+1))
+    merged[-1] = a_t
+    return merged
 
 def get_q_values(model, s_t):
     q_values = np.zeros((number_of_actions, ))
     for i in range(q_values.size):
-        input = merge_state_action(s_t, i)
-        q_values[i] = model.predict(input)
+        q_values[i] = model.predict(merge_state_action(s_t, i))
 
     return q_values
 
 def pacakge_replay(s_t, a_t, r_t, s_t1):
     package = np.zeros((s_t.size*2 + 2))
     package[0:s_t.size] = s_t
-    package[s_t.size+1] = a_t
-    package[s_t.size+2] = r_t
+    package[s_t.size] = a_t
+    package[s_t.size+1] = r_t
     package[-s_t.size:] = s_t1
     return package[None, :]
 
 def unpacakge_replay(package):
     size = (package.size-2)/2
     s_t = package[:size]
-    a_t = package[size+1]
-    r_t = package[size+2]
+    a_t = package[size]
+    r_t = package[size+1]
     s_t1 = package[-size:]
     return (s_t, a_t, r_t, s_t1)
 
 def init_model():
     model = Sequential()
-    model.add(Dense(9, input_shape=(4+1,), activation='relu'))
-    model.add(Dense(9, activation='relu'))
-    model.add(Dense(9, activation='relu'))
+    model.add(Dense(6, input_shape=(length_of_states+1,), activation='relu'))
+    model.add(Dense(6, activation='relu'))
+    model.add(Dense(6, activation='relu'))
     model.add(Dense(1, activation='linear'))
 
     model.compile(optimizer='rmsprop', loss='mse')
