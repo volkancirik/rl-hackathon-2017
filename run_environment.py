6def740852473cd757d8892197c96a37935cbd70
Torsten Wörtwein <twoertwein@gmail.com>
2017-01-14 16:56:43 -0500

    several small changes

--- run_environment.py
+++ run_environment.py
@@ -8,15 +8,14 @@ TODO :
 import gym
 from rl.agents.ac import *
 from rl.agents.dqn import *
+from rl.agents.pq import *
 
-MY_ENV_NAME='Pong-v0'
+MY_ENV_NAME = 'Pong-v0'
 env = gym.make(MY_ENV_NAME)
 OBSERVATION_SPACE = 4 ## could be more than that with concatenation of previous states
 ACTION_SPACE = env.action_space.n
-METHODS = { 'dqn' : QLearningAgent }
+METHODS = {'dqn': QLearningAgent, 'ac': ActorCriticAgent, 'pq': PGagent}
 agent_type = 'dqn'
 
 agent = METHODS[agent_type](state_size = OBSERVATION_SPACE, action_size = ACTION_SPACE)
 agent.train(env)
-
-
