3d3c36325d7c206082cf16bb420523980f410665
Chaitanya Ahuja <ahujachaitanya@gmail.com>
2017-01-14 17:01:28 -0500

    run_environment name bug

--- run_environment.py
+++ run_environment.py
@@ -8,13 +8,13 @@ TODO :
 import gym
 from rl.agents.ac import *
 from rl.agents.dqn import *
-from rl.agents.pq import *
+from rl.agents.pg import *
 
 MY_ENV_NAME = 'Pong-v0'
 env = gym.make(MY_ENV_NAME)
 OBSERVATION_SPACE = 4 ## could be more than that with concatenation of previous states
 ACTION_SPACE = env.action_space.n
-METHODS = {'dqn': QLearningAgent, 'ac': ActorCriticAgent, 'pq': PGagent}
+METHODS = {'dqn': QLearningAgent, 'ac': ActorCriticAgent, 'pg': PGagent}
 agent_type = 'dqn'
 
 agent = METHODS[agent_type](state_size = OBSERVATION_SPACE, action_size = ACTION_SPACE)
