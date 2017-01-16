25d02764ef9a63279cd3a5dff211110b1554c2b3
Torsten Wörtwein <twoertwein@gmail.com>
2017-01-15 19:51:32 -0500

    new features in experience buffer

--- rl/scripts/experience_buffer.py
+++ rl/scripts/experience_buffer.py
@@ -7,10 +7,14 @@ class experience_buffer():
      - sample:  randomly smaple the buffer
      - sample_equal samples all reward types equally
     '''
-    def __init__(self, buffer_size=2000, reward_index=5):
+    def __init__(self, buffer_size=2000, reward_index=5, action_index=4):
         self.buffer = np.empty((0,))
         self.buffer_size = buffer_size
         self.reward_index = reward_index
+        self.action_index = action_index
+
+    def almost_full(self):
+        return self.buffer.shape[0] >= self.buffer_size*0.75
 
     '''
     Adds new experiences (np.array of size entries x length).
@@ -27,20 +31,24 @@ class experience_buffer():
 
         self.buffer = np.append(self.buffer, experience, axis=0)
 
+
     '''
     Adds new experiences (np.array of size entries x length).
-    It replaces the the most frequent rewards first.
+    It replaces the the most frequent rewards&action first.
     '''
     def add_equal(self, experience):
+
+        experience = unique_entries(experience)
+
         number_to_remove = self.buffer.shape[0] + experience.shape[0] - self.buffer_size
 
         while number_to_remove > 0:
             # remove most frequent reward
-            values = self.buffer[:, self.reward_index]
-            types = np.unique(values)
-            hist_fun = np.vectorize(lambda x: np.sum(x == values))
-            hist = hist_fun(types)
-            most_frequent_index = types[np.argmax(hist)] == values
+            values = self.buffer[:, [self.reward_index, self.action_index]]
+            types = unique_entries(values)
+            hist = hist_fun(values, types)
+            print hist
+            most_frequent_index = np.all(types[np.argmax(hist)] == values, 1)
             number_of_most_frequent_reward = np.sum(most_frequent_index)
             remove_index = np.where(most_frequent_index)[0][0:min(number_of_most_frequent_reward, number_to_remove)]
             keep_index = np.setdiff1d(np.arange(self.buffer.shape[0]), remove_index)
@@ -51,7 +59,7 @@ class experience_buffer():
             self.buffer = np.empty(np.concatenate(([0], experience.shape[1:])))
 
         self.buffer = np.append(self.buffer, experience, axis=0)
-
+        self.buffer = unique_entries(self.buffer)
 
     '''
     Samples the buffer randomly
@@ -60,18 +68,18 @@ class experience_buffer():
         return self.buffer[np.random.randint(0, self.buffer.shape[0], number_of_samples), :]
 
     '''
-    Samples the buffer so that each reward has the same probability
+    Samples the buffer so that each reward&action has the same probability
     '''
     def sample_equal(self, number_of_samples):
-        values = self.buffer[:, self.reward_index]
-        types = np.unique(values)
+        values = self.buffer[:, [self.reward_index, self.action_index]]
+        types = unique_entries(values)
 
         results = np.empty((number_of_samples, self.buffer.shape[1]))
         number_inserted = 0
 
         # sample all rewards equally
-        for i in range(types.size):
-            index = np.where(types[i] == values)[0]
+        for i in range(types.shape[0]):
+            index = np.where(np.all(types[i, :] == values, 1))[0]
             new_data = self.buffer[index[np.random.randint(0, index.size, np.int(np.floor(number_of_samples/types.size)))], :]
             results[number_inserted:number_inserted+new_data.shape[0], :] = new_data
             number_inserted += new_data.shape[0]
@@ -81,3 +89,15 @@ class experience_buffer():
             results[number_inserted:, :] = self.sample(number_of_samples - number_inserted)
 
         return results
+
+def unique_entries(data):
+    b = np.ascontiguousarray(data).view(np.dtype((np.void, data.dtype.itemsize * data.shape[1])))
+    _, idx = np.unique(b, return_index=True)
+
+    return data[idx]
+
+def hist_fun(data, x):
+    result = np.empty(x.shape[0])
+    for i in range(x.shape[0]):
+        result[i] = np.sum(np.all(data == x[i, :], 1))
+    return result
